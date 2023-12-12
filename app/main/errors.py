aedd57b984aff701edddcae9380c8b4daf1225f9
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-12-11 20:05:39 -0500

    adding packages to venv

---
+++ app/main/errors.py
@@ -0,0 +1,14 @@
+"""
+Error handler file for Flask app.
+"""
+
+from flask import render_template
+from . import bp
+
+@bp.app_errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@bp.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
