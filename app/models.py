3b69d051de227d05810e3a9e3853b85addec44a9
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-12-13 21:06:26 -0500

    models edit

--- app/models.py
+++ app/models.py
@@ -13,30 +13,6 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f"<User ID: {self.id}, Username: {self.username}>"
-    
-    def is_active(self):
-        """
-        True, as all users are active.
-        """
-        return True
-
-    def get_id(self):
-        """
-        Return the email address to satisfy Flask-Login's requirements.
-        """
-        return self.email
-
-    def is_authenticated(self):
-        """
-        Return True if the user is authenticated.
-        """
-        return self.authenticated
-
-    def is_anonymous(self):
-        """
-        False, as anonymous users aren't supported.
-        """
-        return False
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
