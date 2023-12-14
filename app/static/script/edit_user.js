896697174fa61de15d21880c815618c0984d6e1d
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-12-14 01:58:17 -0500

    js wording

--- app/static/script/edit_user.js
+++ app/static/script/edit_user.js
@@ -1,16 +1,15 @@
 function editUsername() {
-    var name = prompt("Enter your new display name:", "");
-    if (name !== null && name !== "") {
-      document.querySelector('.username').textContent = name;
+  var name = prompt("New display name:", "");
+  if (name !== null && name !== "") {
+    document.querySelector('.username').textContent = name;
 
 
-    }
   }
-  
-  function editAboutMe() {
-    var aboutMe = prompt("Enter your new about me:", "");
-    if (aboutMe !== null && aboutMe !== "") {
-      document.getElementById('about-me-text').textContent = aboutMe;
-    }
+}
+
+function editAboutMe() {
+  var aboutMe = prompt("Tell us about yourself:", "");
+  if (aboutMe !== null && aboutMe !== "") {
+    document.getElementById('about-me-text').textContent = aboutMe;
   }
-  
+}
