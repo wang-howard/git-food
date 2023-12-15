ea78bc48633cd898f615fd66d4560f3311faecfc
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-12-15 16:16:05 -0500

    manage error pages

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,14 +1,22 @@
 from flask import render_template
 from . import main
 
-@main.app_errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-@main.app_errorhandler(500)
-def internal_server_error(e):
-    return render_template("500.html"), 500
+@main.app_errorhandler(401)
+def bad_request(e):
+    return render_template("errors/400.html"), 400
 
 @main.app_errorhandler(401)
 def unauthorized(e):
-    return render_template("401.html"), 401
+    return render_template("errors/401.html"), 401
+
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template("errors/403.html"), 403
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template("errors/404.html"), 404
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template("errors/500.html"), 500
