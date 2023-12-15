4b442756887e6e1c48047e01d9b9370f0ee2fa13
NGUYEN <nnguyenn639@gmail.com>
2023-12-15 13:29:06 -0500

    added modal for collaborators

--- app/static/script/modal.js
+++ app/static/script/modal.js
@@ -1,31 +1,24 @@
-// Get the modal
 var modal = document.getElementById("collaboratorModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("add-collaborator-button");
-
-// Get the elements that close the modal
 var span = document.getElementsByClassName("close")[0];
-var cancelInvite = document.getElementById("cancelInvite");
 
-// When the user clicks on the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks on "No, Cancel", close the modal
-cancelInvite.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+var searchBtn = document.getElementById("searchCollaborator");
+searchBtn.onclick = function() {
+  var username = document.getElementById("collaboratorUsername").value;
+  console.log("Searching for user:", username);
+  // Add your AJAX call or form submission logic here
 }
